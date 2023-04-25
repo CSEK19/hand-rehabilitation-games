62622af20b214a6a8aa0e7ebb3cbd2ab750884c8
phattt <phat.tran.k19@gmail.com>
2023-04-25 15:56:16 +0700

    update utils

--- utils/constants.py
+++ utils/constants.py
@@ -3,3 +3,28 @@ SCREEN_HEIGHT = 720
 FONT_COLOR = (56, 83, 153)
 DEFAULT_COLOR = (64, 61, 57)
 WHITE_COLOR = (255, 255, 255)
+
+
+# Color setting - English
+EASY_COLOR_LIST = ['red', 'blue', 'green']
+EASY_COLOR_CODE_LIST = [(230, 57, 70), (17, 138, 178), (6, 214, 160)]
+EASY_SHAPE_LIST = ['circle', 'square', 'triangle']
+
+MEDIUM_COLOR_LIST = ['red', 'blue', 'green', 'white', 'black']
+MEDIUM_COLOR_CODE_LIST = [(230, 57, 70), (17, 138, 178), (6, 214, 160), (255, 255, 255), (64, 61, 57)]
+MEDIUM_SHAPE_LIST = ['circle', 'square', 'triangle', 'oval']
+
+HARD_COLOR_LIST = ['red', 'blue', 'green', 'white', 'black', 'purple', 'orange']
+HARD_COLOR_CODE_LIST = [(230, 57, 70), (17, 138, 178), (6, 214, 160), (255, 255, 255), (64, 61, 57), (218, 112, 214),
+                        (255, 127, 80)]
+HARD_SHAPE_LIST = ['circle', 'square', 'triangle', 'oval', 'star', 'diamond']
+
+# Color setting - Vietnamese
+VIE_EASY_COLOR_LIST = ['đỏ', 'xanh dương', 'xanh lá']
+VIE_EASY_SHAPE_LIST = ['tròn', 'vuông', 'tam giác']
+
+VIE_MEDIUM_COLOR_LIST = ['đỏ', 'xanh dương', 'xanh lá', 'trắng', 'đen']
+VIE_MEDIUM_SHAPE_LIST = ['tròn', 'vuông', 'tam giác', 'bầu dục']
+
+VIE_HARD_COLOR_LIST = ['đỏ', 'xanh dương', 'xanh lá', 'trắng', 'đen', "tím", "cam"]
+VIE_HARD_SHAPE_LIST = ['tròn', 'vuông', 'tam giác', 'bầu dục', 'ngôi sao', 'kim cương']
--- utils/functions.py
+++ utils/functions.py
@@ -1,30 +1,32 @@
 import pygame
 from utils.constants import *
 
-def paused(screen):
+def paused(screen, enable_Vie_language):
     pause = True
     selection = 0
-    option_list(selection, screen)
+    option_list(selection, screen, enable_Vie_language)
 
     while pause:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
                     selection = (selection + 1) % 2
-                    option_list(selection, screen)
+                    option_list(selection, screen, enable_Vie_language)
                 elif event.key == pygame.K_UP:
                     selection = (selection - 1) % 2
-                    option_list(selection, screen)
+                    option_list(selection, screen, enable_Vie_language)
                 elif event.key == pygame.K_RETURN:
                     return selection
 
-def replay_or_return(selection, screen):
-    popup_width, popup_height = 600, 160
+def replay_or_return(selection, screen, enable_Vie_language):
+    popup_width, popup_height = 800, 180
     popup_surface = pygame.Surface((popup_width, popup_height), pygame.SRCALPHA)
     popup_surface.fill((211, 211, 211, 0))  # set alpha to 0
     font_popup_text = pygame.font.Font('Be_Vietnam_Pro/BeVietnamPro-Black.ttf', 30)
-
-    options = ['Replay', 'Home']
+    if not enable_Vie_language:
+        options = ['Replay', 'Home']
+    else:
+        options = ['Chơi lại', 'Quay lại màn hình chính']
     texts = [font_popup_text.render(text, True, DEFAULT_COLOR) if i != selection else font_popup_text.render(text, True,
                                                                                                              FONT_COLOR)
              for (i, text) in enumerate(options)]
@@ -42,20 +44,27 @@ def replay_or_return(selection, screen):
     screen.blit(popup_surface, popup_rect)
     pygame.display.update()
 
-def option_list(selection, screen):
-    popup_width, popup_height = 600, 160
+def option_list(selection, screen, enable_Vie_language):
+    popup_width, popup_height = 800, 180
     popup_surface = pygame.Surface((popup_width, popup_height), pygame.SRCALPHA)
     popup_surface.fill((211, 211, 211, 255))  # set alpha to 0
     font_popup = pygame.font.Font('Be_Vietnam_Pro/BeVietnamPro-Black.ttf', 40)
     font_popup_text = pygame.font.Font('Be_Vietnam_Pro/BeVietnamPro-Black.ttf', 30)
 
     text_surface = font_popup.render("Game Paused", True, (255, 99, 71))
+    
+    if not enable_Vie_language:
+        text_surface = font_popup.render("Game Paused", True, (255, 99, 71))
+        options = ['Continue', 'Home']
+    else:
+        text_surface = font_popup.render("Trò chơi tạm dừng", True, (255, 99, 71))
+        options = ['Tiếp tục', 'Quay lại màn hình chính']
+
     text_rect = text_surface.get_rect()
     text_rect.centerx = popup_surface.get_rect().centerx
     text_rect.top = 20
     popup_surface.blit(text_surface, text_rect)
 
-    options = ['Continue', 'Home']
     texts = [font_popup_text.render(text, True, DEFAULT_COLOR) if i != selection else font_popup_text.render(text, True,
                                                                                                              FONT_COLOR)
              for (i, text) in enumerate(options)]
